220e4e959c5f43ed640e7abca47c00fef0b9e8e6
frheault <francois.m.rheault@usherbrooke.ca>
2025-07-18 17:19:22 +0000

    fix script

--- modules/local/connectivity/signatures/resources/usr/bin/generate_junctions.py
+++ modules/local/connectivity/signatures/resources/usr/bin/generate_junctions.py
@@ -60,15 +60,15 @@ def main():
     with open(args.in_assignement_files[1], 'r') as f:
         mapping_labels = json.load(f)
     orig_labels = np.squeeze(mapping_labels['orig_label'])
-    new_labels = np.squeeze(mapping_labels['new_label'])
-    mapping_labels = dict(zip(orig_labels, new_labels))
+    # new_labels = np.squeeze(mapping_labels['new_label'])
+    # mapping_labels = dict(zip(orig_labels, new_labels))
 
     # Define LABEL matrix (ensure dtype is appropriate, e.g., int)
     # Using the second LABEL matrix provided in the MATLAB code
-    labels = np.ones((15, 15), dtype=int) * -1
+    matrix_pos = np.ones((15, 15), dtype=int) * -1
     comb_list = np.triu_indices(15, k=0)
     for i, coord in enumerate(zip(*comb_list)):
-        labels[coord] = i + 1
+        matrix_pos[coord] = i + 1
 
     # mat_data = scipy.io.loadmat(args.in_assignement_files[0])
     # B = mat_data['B']
@@ -78,7 +78,7 @@ def main():
     print(f"Loaded {len(all_possibles_signatures)} signatures from "
           f"{args.in_assignement_files[0]}")
 
-    all_signatures = all_possibles_signatures[orig_labels].astype(np.uint8)
+    all_signatures = all_possibles_signatures.astype(float)
     all_signatures_dict = {hash(tuple(row)): i for i,
                            row in enumerate(all_signatures)}
     print(f"Using {len(all_signatures)} signatures from "
@@ -92,8 +92,8 @@ def main():
     wm_data = wm_img.get_fdata().astype(np.float32)
     wm_mask = wm_data > 0.0
 
-    max_label = np.max(labels[labels > 0])
-    tdi_data = np.zeros(wm_data.shape + (max_label,), dtype=float)
+    max_label = np.max(matrix_pos[matrix_pos > 0])
+    tdi_data = np.zeros(wm_data.shape + (max_label,), dtype=np.float16)
 
     assert_headers_compatible(wm_img, [nufo_img])
     print(f"Initialized labels array with shape: {tdi_data.shape}")
@@ -110,29 +110,37 @@ def main():
 
         img = nib.load(tdi_path)
         assert_headers_compatible(wm_img, [img])
-        data = img.get_fdata().astype(np.uint32)
+        data = img.get_fdata()
 
-        label_index = labels[id_1, id_2] - 1
+        label_index = matrix_pos[id_1, id_2] - 1
         tdi_data[..., label_index] = data
-        print(np.max(tdi_data))
+
         count += 1
+        continue
 
     if count != max_label:
         print(f"Warning: Expected {max_label} TDI files, but found {count}.")
 
+    # Voxel-wise normalization of TDI data and thresholding,
+    # contribution below THR is set to 0
+    print("Normalizing TDI files...")
     THR = 0.10
-    mask_sum = np.sum(tdi_data, axis=-1)
-    for ind in np.argwhere(mask_sum > 0):
+    mask_sum = np.sum(tdi_data, axis=-1).astype(float)
+    for ind in tqdm.tqdm(np.argwhere(mask_sum > 0),
+                         total=np.count_nonzero(mask_sum > 0)):
         ind = tuple(ind)
         tmp_tdi_data = tdi_data[ind] / mask_sum[ind]
-        # print(ind, tdi_data[ind], mask_sum[ind], tmp_tdi_data)
         tmp_tdi_data[tmp_tdi_data < THR] = 0
-        tmp_tdi_data /= np.sum(tmp_tdi_data)
-        if mask_sum[ind] > 2:
-            print(ind, tdi_data[ind], mask_sum[ind], tmp_tdi_data)
-        tdi_data[ind] = tmp_tdi_data
+        tmp_sum = np.sum(tmp_tdi_data).astype(float)
+        if tmp_sum < 1e-6:
+            tmp_tdi_data[:] = 0
+        else:
+            tmp_tdi_data /= tmp_sum
+        
+        # This should be ceil to ensure integer values (binarize)
+        tdi_data[ind] = np.ceil(tmp_tdi_data)
 
-    tdi_data[tdi_data > 0] = 1
+    tdi_data = tdi_data.astype(np.uint8)
 
     def _process_voxel(signature):
         """
@@ -145,37 +153,34 @@ def main():
         curr_hash = hash(tuple(signature))
         # Check if the current signature is in the dictionary
         if curr_hash in all_signatures_dict:
-            best_match = all_signatures_dict[curr_hash]
-            return best_match + 1
-        else:
-            for i in range(1, 4):  # Max NUFO is 3
-                # Check if the current signature is a subset of any signature in all_signatures
-                if i == signature[0]:
-                    continue
-                elif curr_hash in all_signatures_dict:
-                    best_match = all_signatures_dict[curr_hash]
-                    return best_match + 1
+            return all_signatures_dict[curr_hash] + 1
 
         # Calculate distances (Cityblock = Manhattan) if not found
         # Too small signature does not have enough information
-        if np.sum(signature[1:]) < 3:
+        curr_sum = np.sum(signature[1:])
+        if curr_sum < 3:
             return -1
 
+        # Filter signatures based on the sum of the signature
         D = cdist(signature[1:].reshape(1, -1), all_signatures[:, 1:],
                   metric='cityblock')[0]
+        
+        best_val = np.min(D)
+        # If the best match is too high, return -1
+        if best_val > curr_sum // 2:
+            return -1
 
-        best_match = np.argmin(D)
-        # min_dist = D[best_match]
-        return best_match + 1
+        return np.argmin(D) + 1
 
     # Flatten except the last dimension
     intersection_mask = np.sum(tdi_data, axis=-1) > 0 & wm_mask
-    tdi_data = tdi_data[intersection_mask]
+    tdi_data = tdi_data[intersection_mask].astype(float)
     nufo_data = nufo_data[intersection_mask]
     num_voxels = np.count_nonzero(intersection_mask)
     labels_ravel = np.zeros_like(nufo_data, dtype=np.int16)
 
     # Voxel-wise processing of signatures
+    print("Processing signatures...")
     for pos in tqdm.tqdm(range(num_voxels), total=num_voxels):
         curr_signature = tdi_data[pos]
         curr_signature = np.insert(curr_signature, 0, nufo_data[pos])
